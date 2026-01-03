0d8b785d61b2d66cda9964cb8d533e2eaf6bfaeb
aymane-bit <aymanekaj2003@gmail.com>
2026-01-03 03:19:26 +0100

    mariadb

---
+++ srcs/requirements/mariadb/mariadb-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+    echo "Error: Missing MariaDB environment variables"
+    exit 1
+fi
+
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+    cat << EOF > /tmp/init_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+FLUSH PRIVILEGES;
+EOF
+
+    /usr/bin/mysqld --user=mysql --bootstrap < /tmp/init_db.sql
+    rm -f /tmp/init_db.sql
+fi
+
+exec /usr/bin/mysqld --user=mysql --console
