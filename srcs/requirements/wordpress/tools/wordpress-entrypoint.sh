fcd62c4e5263685fdb3dd8f182d39ffb6f8a25ec
aymane-bit <aymanekaj2003@gmail.com>
2026-01-05 01:56:30 +0100

    finalv1

---
+++ srcs/requirements/wordpress/tools/wordpress-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+WP_PATH="/var/www/html"
+
+# 1) Wait for MariaDB to be ready
+until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+    sleep 1
+done
+
+cd $WP_PATH
+
+# 2) Create wp-config.php if it does not exist
+if [ ! -f wp-config.php ]; then
+    cp wp-config-sample.php wp-config.php
+
+    sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" wp-config.php
+    sed -i "s/username_here/${WORDPRESS_DB_USER}/" wp-config.php
+    sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" wp-config.php
+    sed -i "s/localhost/${WORDPRESS_DB_HOST}/" wp-config.php
+fi
+
+# 3) Install WordPress if not installed
+if ! wp core is-installed --allow-root; then
+    wp core install \
+        --url="${WP_URL}" \
+        --title="${WP_TITLE}" \
+        --admin_user="${WP_ADMIN_USER}" \
+        --admin_password="${WP_ADMIN_PASSWORD}" \
+        --admin_email="${WP_ADMIN_EMAIL}" \
+        --allow-root
+fi
+
+# 4) Start PHP-FPM
+mkdir -p /run/php
+chown www-data:www-data /run/php
+
+exec php-fpm7.4 -F
